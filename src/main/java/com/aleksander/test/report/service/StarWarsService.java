adade0e0610e8fde1447ac3e87e5d6c00fa8b806
Aleksander Radolak <alekradolak@gmail.com>
2020-03-03 13:00:59 +0100

    Refactor to more generic consuming

--- src/main/java/com/aleksander/test/report/service/StarWarsService.java
+++ src/main/java/com/aleksander/test/report/service/StarWarsService.java
@@ -2,19 +2,18 @@ package com.aleksander.test.report.service;
 
 import com.aleksander.test.report.dto.Person;
 import com.aleksander.test.report.dto.Planet;
-import com.aleksander.test.report.dto.response.PeopleResponse;
-import com.aleksander.test.report.dto.response.PlanetResponse;
+import com.aleksander.test.report.dto.response.QueryResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class StarWarsService {
     private final static String PLANETS_URL = "https://swapi.co/api/planets/";
@@ -22,31 +21,49 @@ public class StarWarsService {
 
     private final ApiConsumer apiConsumer;
 
+    private final ObjectMapper objectMapper;
+
 
     public List<Planet> getPlanetsByPhrase(String phrase) {
-        List<Planet> planets = new ArrayList<>();
-        String url = buildQueryUrl(PLANETS_URL, phrase);
-
-        do {
-            PlanetResponse response = apiConsumer.getResponse(url, PlanetResponse.class);
-            url = Objects.nonNull(response.getNext()) ? buildQueryUrl(response.getNext()) : null;
-            planets.addAll(response.getResults());
-        } while (Objects.nonNull(url));
-
-        return planets;
+        return getByPhrase(PLANETS_URL, phrase, Planet.class);
     }
 
     public List<Person> getPeopleByPhrase(String phrase) {
-        List<Person> people = new ArrayList<>();
-        String url = buildQueryUrl(PEOPLE_URL, phrase);
+        return getByPhrase(PEOPLE_URL, phrase, Person.class);
+    }
+
+    /**
+     * Make get request to endpoint with search query based on phrase to fetch data from all pages
+     * @param endpoint
+     * @param phrase
+     * @param type of object to map
+     * @param <T>
+     * @return list of all objects matching given query phrase
+     */
+    private <T> List<T> getByPhrase(String endpoint, String phrase, Class<T> type) {
+        List<T> list = new ArrayList<>();
+        String url = buildQueryUrl(endpoint, phrase);
 
         do {
-            PeopleResponse response = apiConsumer.getResponse(url, PeopleResponse.class);
+            QueryResponse response = apiConsumer.getResponse(url, QueryResponse.class);
             url = Objects.nonNull(response.getNext()) ? buildQueryUrl(response.getNext()) : null;
-            people.addAll(response.getResults());
+            list.addAll(mapToListOfObjectType(response.getResults(), type));
         } while (Objects.nonNull(url));
 
-        return people;
+        return list;
+    }
+
+    /**
+     * Map list containing LinkedHashMap<String, String> to object of given type
+     * @param fromList
+     * @param type
+     * @param <T>
+     * @return mapped list
+     */
+    private <T> List<T> mapToListOfObjectType(List<Object> fromList, Class<T> type) {
+        return fromList.stream()
+                .map(ele -> objectMapper.convertValue(ele, type))
+                .collect(Collectors.toList());
     }
 
     private String buildQueryUrl(String endpoint) {
