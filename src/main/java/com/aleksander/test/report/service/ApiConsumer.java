4fecb036acc71c72983a7ac29ef649c00142cced
Aleksander Radolak <alekradolak@gmail.com>
2020-03-05 15:57:36 +0100

    Fix building and testing application through maven

--- src/main/java/com/aleksander/test/report/service/ApiConsumer.java
+++ src/main/java/com/aleksander/test/report/service/ApiConsumer.java
@@ -2,8 +2,7 @@ package com.aleksander.test.report.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
@@ -17,7 +16,7 @@ public class ApiConsumer {
     private final RestTemplate restTemplate;
 
     protected <T> T getResponse(String url, Class<T> type) {
-        ResponseEntity<T> response = restTemplate.getForEntity(url, type);
+        ResponseEntity<T> response = restTemplate.exchange(url, HttpMethod.GET, getHttpEntity(), type);
         HttpStatus statusCode = response.getStatusCode();
 //      Handle other http status?
         return (statusCode == HttpStatus.OK) ? response.getBody() : null;
@@ -26,6 +25,18 @@ public class ApiConsumer {
     @Async("asyncExecutor")
     protected <T> CompletableFuture<ResponseEntity<T>> getResponseAsync(String url, Class<T> type) {
         log.debug("Request: " + url);
-        return CompletableFuture.completedFuture(restTemplate.getForEntity(url, type));
+        return CompletableFuture.completedFuture(restTemplate.exchange(url, HttpMethod.GET, getHttpEntity(), type));
+    }
+
+
+    /**
+     * Add user agent to mock browser and system data, required to build and test application through maven. Otherwise SWAPI return 403.
+     * https://stackoverflow.com/questions/46440982/consuming-rest-works-with-curl-but-does-not-work-when-i-am-usiing-resttemplate
+     * @return
+     */
+    private HttpEntity<Object> getHttpEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:55.0) Gecko/20100101 Firefox/55.0");
+        return new HttpEntity<>(headers);
     }
 }
